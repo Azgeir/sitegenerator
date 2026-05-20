843953d3d55543e96a061d681e81d0076ce46ca9
Azgeir <askewulf@gmail.com>
2026-05-20 20:07:35 +0200

    Add test for copy

--- sitegenerator/src/main.py
+++ sitegenerator/src/main.py
@@ -4,19 +4,19 @@ import os
 
 
 def main():
-    textnode = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(textnode)
+    copy_contents("static", "public")
 
-def copy_contents(src, dir):
-    if os.path.exists(dir):
-        shutil.rmtree(dir)
-    os.mkdir(dir)
-    copy(src, dir)
+def copy_contents(src, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    os.mkdir(dest)
+    copy(src, dest)
 
-def copy(src, dir):
+def copy(src, dest):
     for file in os.listdir(src):
         file_path = os.path.join(src, file)
-        target_path = os.path.join(dir, file)
+        target_path = os.path.join(dest, file)
+        print(f"Copying {file_path} to {target_path}")
         if not os.path.isfile(file_path):
             os.mkdir(target_path)
             copy(file_path, target_path)
